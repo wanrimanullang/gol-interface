e065e98a9572028f49c36d88e68ef31ff124c092
werry216 <87049471+werry216@users.noreply.github.com>
2022-01-04 20:03:40 +0200

    fixed

--- src/views/Home/components/Footer.tsx
+++ src/views/Home/components/Footer.tsx
@@ -67,7 +67,7 @@ const topLeftImage = {
   path: '/images/home/flying-pancakes/',
   attributes: [
     // { src: '1-bottom', alt: 'Gol flying on the bottom' },
-    { src: '1-left', alt: 'Gol flying on the left' },
+    { src: 'avar-left', alt: 'Gol flying on the left' },
     // { src: '1-top', alt: 'Gol flying on the top' },
   ],
 }
@@ -77,7 +77,7 @@ const bottomRightImage = {
   attributes: [
     // { src: '1-left', alt: 'Gol flying on the left' },
     // { src: '1-bottom', alt: 'Gol flying on the bottom' },
-    { src: '1-top', alt: 'Gol flying on the top' },
+    { src: 'avar-top', alt: 'Gol flying on the top' },
     // { src: '2-right', alt: 'Gol flying on the right' },
   ],
 }
